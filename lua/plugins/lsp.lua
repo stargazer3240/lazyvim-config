018eed92871859c9719a2efbd39097f7bfe37fa4
Stargazer3240 <112516550+Stargazer3240@users.noreply.github.com>
2024-07-26 15:52:50 -0300

    Add LazyVim Extras and some lsps

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,22 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {
+          cmd = {
+            "clangd",
+            "--background-index",
+            "--clang-tidy",
+            "--header-insertion=iwyu",
+            "--completion-style=detailed",
+            "--function-arg-placeholders",
+            "--fallback-style=google",
+          },
+        },
+        cssls = {},
+        html = {},
+      },
+    },
+  },
+}
